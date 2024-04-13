72649e5a9ac90586bd662b42f42c4638584caacd
emonkhan500 <khdilo47@gmail.com>
2024-04-13 20:18:03 +0600

    tailwind,daisy setup complete

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
-            <h1>This is home</h1>
+
+            <h1 className='text-4xl'>This is home</h1>
         </div>
     );
 };
